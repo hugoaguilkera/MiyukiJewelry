86cf34a2913bfcf722609b0937f1f1284ae08214
Hugo Aguilera <hugo.aguilera@pactra.com>
2026-02-13 12:28:10 -0600

    fix tailwind content paths for production

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography")
+  ],
+}
