4df0a8e341caae7d7bd4d5c8b90c3c53e4597a98
Hugo Aguilera <hugo.aguilera@pactra.com>
2026-02-17 13:04:01 -0600

    simplify products api

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,12 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+import * as schema from "@shared/schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
-const sql = neon(process.env.DATABASE_URL, {
-  fetch: fetch, // 👈 ESTA LÍNEA ES LA CLAVE
-});
+const sql = neon(process.env.DATABASE_URL);
 
-export const db = drizzle(sql);
+export const db = drizzle(sql, { schema });
+
