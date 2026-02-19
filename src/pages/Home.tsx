c39b6a7d0830029eaf6576fbb6e9e5156f298afa
Hugo Aguilera <hugo.aguilera@pactra.com>
2026-02-19 13:20:34 -0600

    test cloudinary image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,8 @@
-import Hero from "../components/home/Hero";
-import Categories from "../components/home/Categories";
-import ProductGallery from "../components/home/ProductGallery";
-import Testimonials from "../components/home/Testimonials";
-import ContactSection from "../components/home/ContactSection";
-
-const Home = () => {
-  return (
-    <>
-      <Hero />
-      <Categories />
-      <ProductGallery />
-      <Testimonials />
-      <ContactSection />
-    </>
-  );
-};
-
-export default Home;
+<div style={{ padding: "40px" }}>
+  <h2>Prueba Imagen Cloudinary</h2>
+  <img 
+    src="https://res.cloudinary.com/dnv4hrn3y/image/upload/v1771013713/pulsera_dorada_con_corazones-removebg-preview-Photoroom3_LE_auto_x4_light_ai_bdtfm7.png"
+    alt="Pulsera dorada"
+    style={{ width: "300px" }}
+  />
+</div>
